e08f1c6951690e9a1ac3c9a4a16712664661a97e
Samark Chaisanguan <samarkchsngn@gmail.com>
2016-09-01 16:52:07 +0700

    add route basic route

--- max/js/main.js
+++ max/js/main.js
@@ -1,8 +1,21 @@
 // (function){}());
 // basic format function on angular type.
 (function() {
-	'use stric';
-	var appEggLab = angular.module('appEggLab', []);
+	"use strict";
+	var appEggLab = angular.module('appEggLab', ['ngRoute']);
+	
+	appEggLab.config(function($routeProvider) {
+		$routeProvider.when('/index', {
+			templateUrl: 'display.html',
+			controller: 'mainController'
+		}).when('/google',{
+			templateUrl: 'js/index2.html',
+			controller: 'reqController'
+		}).otherwise({
+			redirectTo: '/index'
+		});
+	});
+	/** controller simple display  */
 	appEggLab.controller('mainController', ['$scope', function($scope) {
 		$scope.name = 'samark chaisanguans';
 		// $scope.list = 'samark chaisanguan';
@@ -21,5 +34,9 @@
 			}
 		};
 		$scope.list = objectJson;
+	}]);
+	/** controller simple 2 reqest */
+	appEggLab.controller('reqController', ['$scope',function($scope){
+		alert('ok');
 	}]);
 }());
