b28b4785756f2ec133c30b635d737e0be1416450
Kit Chittawanij <sukit.chi@ascendcorp.com>
2016-09-01 19:57:19 +0700

    js

--- kit/js/main.js
+++ kit/js/main.js
@@ -15,7 +15,11 @@
 	/** controller simple display  */
 	appEggLab.controller('mainController', ['$scope', function($scope) {
 		$scope.submit = function(list){
-			console.log(list);
+			if(list){
+				console.log('success');
+			} else {
+				console.log('error');
+			}
 		}
 		$scope.name = '';
 		// $scope.list = 'samark chaisanguan';
